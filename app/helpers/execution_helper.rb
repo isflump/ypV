891646b2201b61871765a7a3e618543edfc5eabb
Yizhong JI <yizhongji@gmail.com>
2014-07-29 16:12:46 -0400

    change on execution report page

--- app/helpers/execution_helper.rb
+++ app/helpers/execution_helper.rb
@@ -1,14 +1,18 @@
 module ExecutionHelper
+  def parse_exception_to_error(exception)
+    exception.split(/^\s*(_\s?)+\s*$/).last.split("\n").collect{|line| line if line =~/^E\s*.*/ }.join("\n").strip()
+  end
+
   def parse_exception_to_user_case(exception)
     result=""
-    exception.split("\n").each{|line|
-        if line.empty?
-          result=result+"\n"
-          next
-        end
-        break if line.match(/^\s*(_\s?)+\s*$/)
-        result=result+"\n"+line
+    return result if !exception
+    isInclude=false
+    exception.split(/^\s*(_\s?)+\s*$/)[0].split("\n").each{|line|
+
+        isInclude=true if line =~ /^\s*(@pytest.mark|def).*$/
+        result=result+"\n"+line if isInclude
     }
     result
+    #exception.split(/^\s*(_\s?)+\s*$/)[0].include?('@pytest') ? exception.split(/^\s*(_\s?)+\s*$/)[0].gsub(exception.match(/(^[^\-]*?)@pytest/)[1],'').gsub(/^@pytest/,exception.split(/^\s*(_\s?)+\s*$/)[0].match(/^.*?@pytest/)[0]) : exception.split(/^\s*(_\s?)+\s*$/)[0].gsub(exception.match(/(^[^\-]*?)def\s/)[1],'').gsub(/^def\s/,exception.split(/^\s*(_\s?)+\s*$/)[0].match(/^.*?def\s/)[0])
   end
 end
